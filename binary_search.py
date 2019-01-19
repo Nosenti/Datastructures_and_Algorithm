c1751ee2bd06f1f3f103cc6740233e4de1d7dc6e
Innocent <inno100os@gmail.com>
2019-01-19 09:36:41 +0200

    First trial

---
+++ binary_search.py
@@ -0,0 +1,22 @@
+
+arr = [1,2,3,4,5,6,7]
+target = 6
+
+def search_index():
+
+    max = len(arr)-1
+    min = 0
+    while (min <= max):
+
+        midpoint = int((max + min)/2)
+        if  arr[midpoint] == target:
+            return midpoint
+        elif target > arr[midpoint]:
+            min = midpoint + 1
+        else:
+            max = midpoint - 1
+
+    return -1
+print(search_index())
+
+
